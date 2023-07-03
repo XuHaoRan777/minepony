65b56c8925d5d1be94cb93095a197c3a4c578d46
XuHaoRan777 <2901985523@qq.com>
2023-07-03 11:23:26 +0800

    路径更新

--- ruoyi-system/src/main/java/com/ruoyi/system/utils/Constants.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/utils/Constants.java
@@ -6,6 +6,12 @@ public class Constants {
     // 本地
     public static final String Promotional_Image_PATH = "F:\\JAVA_project\\minepony_file\\promotionalImage\\";
     // 正式
-    // public static final String Promotional_Image_PATH = "F:\\JAVA_project\\minepony_file\\promotionalImage\\";
+    // public static final String Promotional_Image_PATH = "";
+
+    // 服务包文件的路径
+    // 本地
+    public static final String SERVER_FILE_PATH = "F:\\JAVA_project\\minepony_file\\serverFile\\";
+    // 正式
+    // public static final String Promotional_Image_PATH = "";
 
 }
